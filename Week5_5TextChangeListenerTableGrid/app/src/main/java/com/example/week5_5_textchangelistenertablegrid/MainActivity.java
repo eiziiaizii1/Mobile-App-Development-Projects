b65f1d3701c9ae2d7f8443ea4f12274dc37031b9
Aziz Önder <azizonder05@gmail.com>
2025-03-08 00:49:50 +0300

    week5-5 is added

---
+++ Week5_5TextChangeListenerTableGrid/app/src/main/java/com/example/week5_5_textchangelistenertablegrid/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.week5_5_textchangelistenertablegrid;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText et;
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        et = findViewById(R.id.et1);
+        tv = findViewById(R.id.tv1);
+
+        et.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                tv.setText("" + et.getText().toString());
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //tv.setText("" + et.getText().toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //tv.setText("" + et.getText().toString());
+            }
+        });
+    }
+}
